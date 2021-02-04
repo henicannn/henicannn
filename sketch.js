f43485cc6c736df9b6a4501a26c57cddf2ee7c40
henipatel8 <hmanavadaria@gmail.com>
2021-02-04 14:26:56 -0500

    p5.js animation added

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(1920, 969, WEBGL);
+    c1 = color(200);
+    c2 = color(255, 255, 255);
+
+    for (let y = 0; y < height; y++) {
+        n = map(y, 0, height, 0, 1);
+        let newc = lerpColor(c1, c2, n);
+        stroke(newc);
+        line(0, y, width, y);
+    }
+}
+
+function draw() {
+    background(37, 41, 52);
+    rotateY(frameCount * 0.01);
+
+    for (let j = 0; j < 5; j++) {
+        push();
+        for (let i = 0; i < 80; i++) {
+            translate(
+                sin(frameCount * 0.001 + j) * 100,
+                sin(frameCount * 0.001 + j) * 100,
+                i * 0.1
+            );
+            rotateZ(frameCount * 0.002);
+            push();
+            sphere(1, 6, 4);
+            pop();
+            // fill(25, 80, 60, 0.1)
+        }
+        pop();
+    }
+}
