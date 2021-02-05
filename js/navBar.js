9de6877a3fa8bf4aeb79c05d9f11d2f2a37ce706
henipatel8 <hmanavadaria@gmail.com>
2021-02-04 19:53:05 -0500

    Fade effect added

---
+++ js/navBar.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+        $('#mainNav').fadeIn();
+    } else {
+        $('#mainNav').fadeOut();
+    }
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+        $('#homePage').fadeOut();
+    } else {
+        $('#homePage').fadeIn();
+    }
+});
