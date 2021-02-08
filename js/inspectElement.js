c58ac10032ddb258b7bb605bbd57c09eac2ae13d
henipatel8 <hmanavadaria@gmail.com>
2021-02-08 01:03:30 -0500

    responsive update

---
+++ js/inspectElement.js
@@ -0,0 +1,15 @@
+var message = "Function Disabled For Security Purpose";
+
+function rtclickcheck(keyp) {
+    if (navigator.appName == "Netscape" && keyp.which == 3) {
+        alert(message);
+        return false;
+    }
+
+    if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) {
+        alert(message);
+        return false;
+    }
+}
+
+document.onmousedown = rtclickcheck;
