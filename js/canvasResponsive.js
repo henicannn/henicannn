eefa83464a6a51a4b4159572abf9b65e7338ee75
henipatel8 <hmanavadaria@gmail.com>
2021-02-11 19:11:54 -0500

    node.js app converted

--- js/canvasResponsive.js
+++
@@ -1,9 +0,0 @@
-function resize() {
-    $("#canvas").outerHeight($(window).height() - $("#canvas").offset().top - Math.abs($("#canvas").outerHeight(true) - $("#canvas").outerHeight()));
-}
-$(document).ready(function() {
-    resize();
-    $(window).on("resize", function() {
-        resize();
-    });
-});
--- js/main.js
+++
@@ -1,4 +0,0 @@
-function redirectIt(obj) {
-    var goToLink = obj.getAttribute("href");
-    window.location.href = goToLink;
-}
